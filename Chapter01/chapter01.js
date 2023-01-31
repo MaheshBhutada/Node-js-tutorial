17e22efdc65a35bb8324e9fcd71f805cbe6a0e07
Mahesh <mrbhutada787@gmail.com>
2023-01-31 23:12:08 +0530

    add the time Interval function in code

---
+++ Chapter01/chapter01.js
@@ -0,0 +1,11 @@
+/*
+__dirname - path to current directory.
+__filename - file name.
+require - function to use modules (CommanJS).
+module - info about current module (file).
+process - info about environment where the program is executed.
+
+*/
+setInterval(()=>{
+    console.log("heloo world");
+})
